93c8025cc9f7e66bffa73cc087662153afb7a5ba
VedantSelmokar <vedant.selmokar@gmail.com>
2025-01-13 19:57:12 +0530

    Create Create terraform_s3_bucket_with_variables.tf

---
+++ Create terraform_s3_bucket_with_variables.tf
@@ -0,0 +1,37 @@
+# Define provider
+provider "aws" {
+  region = var.region
+}
+
+# Define an S3 bucket resource
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+  acl    = var.acl
+
+  # Optional: Enable versioning
+  versioning {
+    enabled = var.enable_versioning
+  }
+}
+
+# Define variables
+variable "region" {
+  description = "The AWS region to create the S3 bucket in."
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket."
+  type        = string
+}
+
+variable "acl" {
+  description = "The ACL to apply to the S3 bucket."
+  default     = "private"
+}
+
+variable "enable_versioning" {
+  description = "Whether to enable versioning for the S3 bucket."
+  type        = bool
+  default     = true
+}
