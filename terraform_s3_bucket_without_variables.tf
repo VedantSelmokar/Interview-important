06ceeda34fa7c6441c5650c5d0142f26ccb779b7
VedantSelmokar <vedant.selmokar@gmail.com>
2025-01-13 20:14:45 +0530

    Update terraform_s3_bucket_without_variables.tf

--- terraform_s3_bucket_without_variables.tf
+++ terraform_s3_bucket_without_variables.tf
@@ -3,9 +3,11 @@ provider "aws" {
   region = "us-east-1" # Change to your desired region
 }
 
-# Create an S3 bucket
-  resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name" # Replace with a globally unique bucket name
-  acl = "private"   # Apply bucket policy for public access prevention
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
- 
